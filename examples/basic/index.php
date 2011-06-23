ba421b38d234a196b79217fba93b9b3c940dfcfe
Steve Clay <steve@mrclay.org>
2011-06-22 23:46:36 -0400

    tiny cleanup

--- examples/basic/index.php
+++ examples/basic/index.php
@@ -2,18 +2,15 @@
 
 // the "SP"
 require '_inc.php';
-
 $sp = new Shibalike\SP(getStateManager(), getUrlConfig());
 $sp->requireValidUser();
 
-if (empty($_SERVER['REMOTE_USER'])) {
-    die('Something went wrong. You should never see this.');
-}
 
 
 // your app's shibboleth auth module here
 
 $username = $_SERVER['REMOTE_USER'];
+
 
 // the "application"
 
@@ -22,5 +19,3 @@ header('Content-Type: text/html;charset=utf-8');
 echo "<h1>Hello, " . htmlspecialchars($_SERVER['displayname'], ENT_QUOTES, 'UTF-8') . "!</h1>";
 
 echo "<p><a href='idp.php?logout'>Sign out</a></p>";
-
-
